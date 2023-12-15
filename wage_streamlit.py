71e1dafa313b1a5769a17341ba7c4bdbcd0e226a
shoya_suzuki <71615819+Shoya-cyber@users.noreply.github.com>
2023-12-15 17:36:54 +0900

    Update wage_streamlit.py

--- wage_streamlit.py
+++ wage_streamlit.py
@@ -7,9 +7,9 @@ import plotly.express as px
 
 st.title('日本の賃金のデータダッシュボード')
 
-df_jp_ind = pd.read_csv('./csv_data/産業構造マップ_雇用_一人当たり賃金_全国_全産業.csv', encoding='shift_jis')
-df_jp_category = pd.read_csv('./csv_data/産業構造マップ_雇用_一人当たり賃金_全国_大分類.csv', encoding='shift_jis')
-df_pref_ind = pd.read_csv('./csv_data/産業構造マップ_雇用_一人当たり賃金_都道府県_全産業.csv', encoding='shift_jis')
+df_jp_ind = pd.read_csv('jp_wage_dashboard/csv_data/産業構造マップ_雇用_一人当たり賃金_全国_全産業.csv', encoding='shift_jis')
+df_jp_category = pd.read_csv('jp_wage_dashboard/csv_data/産業構造マップ_雇用_一人当たり賃金_全国_大分類.csv', encoding='shift_jis')
+df_pref_ind = pd.read_csv('jp_wage_dashboard/csv_data/産業構造マップ_雇用_一人当たり賃金_都道府県_全産業.csv', encoding='shift_jis')
 
 ###########################################################
 
